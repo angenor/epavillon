6cdb89fc6cad07bba9fe1cff98ccd813b1b7df99
Issa <badosa2@yahoo.fr>
2025-10-11 18:05:48 +0000

    OK

--- src/components/admin/ActivityReviewSidebar.vue
+++ src/components/admin/ActivityReviewSidebar.vue
@@ -111,6 +111,7 @@
             <svg class="w-3.5 h-3.5" fill="currentColor" viewBox="0 0 20 20">
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
             </svg>
+            <span v-if="totalRatedActivities > 0" class="font-semibold">{{ totalRatedActivities }}</span>
           </button>
         </div>
       </div>
@@ -400,6 +401,10 @@ const filteredActivities = computed(() => {
 
 const totalUnreadComments = computed(() => {
   return activities.value.reduce((sum, activity) => sum + (activity.comments_count || 0), 0)
+})
+
+const totalRatedActivities = computed(() => {
+  return activities.value.filter(a => a.user_rating !== null && a.user_rating !== undefined).length
 })
 
 const currentIndex = computed(() => {
