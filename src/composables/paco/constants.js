dd95b3c0b8cb1947fa16a27db4d93256d258c418
Angenor <angenor99@gmail.com>
2026-04-10 19:12:45 +0000

    OK

--- src/composables/paco/constants.js
+++ src/composables/paco/constants.js
@@ -1,4 +1,4 @@
 // PACO Webinar constants — isolated for easy removal post-event
 export const PACO_EVENT_ID = '00000000-0000-4000-a000-00000000e001'
 export const PACO_ACTIVITY_ID = '00000000-0000-4000-a000-00000000a002'
-export const PACO_TEAMS_LINK = 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_MWViNGEyZGItYmJkZi00OTkxLWFjY2UtNTU5N2ViMzlmNjg0%40thread.v2/0?context=%7b%22Tid%22%3a%227c0a7f1c-bfe8-498e-a499-b9c398fb8cba%22%2c%22Oid%22%3a%220a469be3-796f-4848-ba91-8d1611e1e8fb%22%7d' // À configurer par l'organisateur avec le lien Teams réel
+export const PACO_TEAMS_LINK = 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_OWM1MmEzYjMtNTM4My00ODU5LWI3YmQtOGZkZDcwYmU2YjNj%40thread.v2/0?context=%7b%22Tid%22%3a%227c0a7f1c-bfe8-498e-a499-b9c398fb8cba%22%2c%22Oid%22%3a%220a469be3-796f-4848-ba91-8d1611e1e8fb%22%7d' // À configurer par l'organisateur avec le lien Teams réel
