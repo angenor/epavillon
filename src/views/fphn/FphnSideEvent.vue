dca6f5f9c18201223a74b6ff9870e827dd943b57
Angenor <angenor99@gmail.com>
2026-07-10 18:17:07 +0000

    ok

--- src/views/fphn/FphnSideEvent.vue
+++ src/views/fphn/FphnSideEvent.vue
@@ -381,15 +381,14 @@ const partners = [
 // Photos : public/images/fphn2026/speakers/<fichier>
 // (le placeholder à initiales disparaît dès que le fichier existe)
 const speakers = [
-  { name: 'M. Michel Xavier Biang', roleKey: 'fphn.speakers.roles.biang', org: 'OIF', photo: '/images/fphn2026/speakers/biang.jpg', initials: 'MB' },
-  { name: 'M. Thomas Schnoell', roleKey: 'fphn.speakers.roles.schnoell', org: 'OCDE', photo: '/images/fphn2026/speakers/schnoell.jpg', initials: 'TS' },
+  { name: 'S.E. M. Michel Xavier Biang', roleKey: 'fphn.speakers.roles.biang', org: 'OIF', photo: '/images/fphn2026/speakers/Photo_SEM_Michel_Xavier_Biang.jpg', initials: 'MB' },
+  { name: 'S.E. M. Thomas Schnöll', roleKey: 'fphn.speakers.roles.schnoell', org: 'OCDE', photo: '/images/fphn2026/speakers/schnoell.jpg', initials: 'TS' },
   { name: 'M. Frantisek Ruzicka', roleKey: 'fphn.speakers.roles.ruzicka', org: 'OCDE', photo: '/images/fphn2026/speakers/ruzicka.jpg', initials: 'FR' },
-  { name: 'M. Juan Casado-Asensio', roleKey: 'fphn.speakers.roles.casado', org: 'OCDE', photo: '/images/fphn2026/speakers/casado.jpg', initials: 'JC' },
+  { name: 'S.E. Mme Louise Pierrette Mvono', roleKey: 'fphn.speakers.roles.gabon', org: 'Gabon', photo: '/images/fphn2026/speakers/Photo_SEMme_Louise_Pierrette_Mvono.jpg', initials: 'LM', tbc: true },
+  { name: 'S.E. M. Paruyr Hovhannisyan', roleKey: 'fphn.speakers.roles.armenia', org: 'Arménie', photo: '/images/fphn2026/speakers/Photo_Paruyr_Hovhannisyan.jpg', initials: 'PH' },
+  { nameKey: 'fphn.speakers.names.france', roleKey: 'fphn.speakers.roles.france', org: 'France', photo: '/images/fphn2026/speakers/france.jpg', initials: 'FR', tbc: true },
+  { name: 'M. Robert Krech', roleKey: 'fphn.speakers.roles.adaptationFund', org: 'Fonds d\'adaptation', photo: '/images/fphn2026/speakers/Photo_Robert_Krech.jpg', initials: 'RK' },
   { name: 'M. Tounao Kiri', roleKey: 'fphn.speakers.roles.kiri', org: 'IFDD-OIF', photo: '/images/fphn2026/speakers/kiri.jpg', initials: 'TK' },
-  { nameKey: 'fphn.speakers.names.armenia', roleKey: 'fphn.speakers.roles.armenia', photo: '/images/fphn2026/speakers/armenie.jpg', initials: 'AM', tbc: true },
-  { nameKey: 'fphn.speakers.names.france', roleKey: 'fphn.speakers.roles.france', photo: '/images/fphn2026/speakers/france.jpg', initials: 'FR', tbc: true },
-  { nameKey: 'fphn.speakers.names.madagascar', roleKey: 'fphn.speakers.roles.madagascar', photo: '/images/fphn2026/speakers/madagascar.jpg', initials: 'MG', tbc: true },
-  { nameKey: 'fphn.speakers.names.adaptationFund', roleKey: 'fphn.speakers.roles.adaptationFund', photo: '/images/fphn2026/speakers/fonds-adaptation.jpg', initials: 'FA', tbc: true },
 ]
 
 const missingPhotos = reactive(new Set())
